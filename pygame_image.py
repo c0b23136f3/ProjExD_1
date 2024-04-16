2f43059bb95e49a84d494cd5eccc6971c79e1480
MochizukiYuchin <c0b23136f3@edu.teu.ac.jp>
2024-04-16 18:01:30 +0900

    演習１終了

--- pygame_image.py
+++ pygame_image.py
@@ -21,13 +21,15 @@ def main():
             if event.type == pg.QUIT: return
         key_lst=pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,+1))
-        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((1,-1))
+        elif key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((1,+1))
+        elif key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1,0))
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1,0))
+        elif key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+2,0))
+        else:
+            kk_rct.move_ip((-1,0))
         x=tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
